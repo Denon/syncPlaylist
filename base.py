e57ce6e0ca34baca329b09057d16ee5aa975bd98
dahong.wu <dahong.wu@ele.me>
2017-06-19 21:40:46 +0800

    restruct

---
+++ base.py
@@ -0,0 +1,86 @@
+import os
+import traceback
+import json
+import signal
+from selenium import webdriver
+from selenium.webdriver.support import ui
+from selenium.webdriver.chrome.options import Options
+from settings import *
+from utils import _print
+
+
+class Config(object):
+    def __init__(self):
+        with open("config.json", 'r') as f:
+            account_json = f.read()
+        self.config = json.loads(account_json)
+
+    @property
+    def qq_account(self):
+        return self.config["qq_account"]
+
+    @property
+    def qq_password(self):
+        return self.config["qq_password"]
+
+    @property
+    def wy_playlist_url(self):
+        return self.config["wy_playlist_url"].replace('#', 'm')
+
+    @property
+    def qq_playlist_name(self):
+        return self.config["qq_playlist_name"]
+
+
+class BaseSpider(object):
+    def __init__(self):
+        self.browse = None
+        self.source_playlist = None
+        self.target_playlist_tag = None
+        self.success_list = list()
+        self.failed_list = list()
+        os.environ["webdriver.chrome.driver"] = chrome_driver_path
+        os.environ["webdriver.phantomjs.driver"] = phantomjs_driver_path
+        # chromedriver = chrome_driver_path
+        phantomjs_driver = phantomjs_driver_path
+
+        opts = Options()
+        opts.add_argument("user-agent={}".format(headers["User-Agent"]))
+        # browser = webdriver.Chrome(chromedriver)
+        browser = webdriver.PhantomJS(phantomjs_driver)
+        self.browser = browser
+        self.wait = ui.WebDriverWait(self.browser, 5)
+        self.config = Config()
+
+    def prepare(self):
+        pass
+
+    def get_source_playlist(self):
+        pass
+
+    def get_target_playlist(self):
+        pass
+
+    def sync_song(self):
+        pass
+
+    def print_result(self):
+        print "total success:{}".format(len(self.success_list))
+        print "total failed:{}, detail:".format(len(self.failed_list))
+        for failed in self.failed_list:
+            _print(failed)
+
+    def run(self):
+        try:
+            self.prepare()
+            self.get_source_playlist()
+            self.get_target_playlist()
+            self.sync_song()
+        except Exception:
+            traceback.print_exc()
+        finally:
+            self.print_result()
+            self.browser.service.process.send_signal(signal.SIGTERM)  # kill the specific phantomjs child proc
+            self.browser.quit()
+
+

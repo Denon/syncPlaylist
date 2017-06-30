3d50712ce92df3ec54ef95a52e7d3e82d6c80481
dahong.wu <dahong.wu@ele.me>
2017-06-30 17:06:12 +0800

    simplify setting

--- WYtoQQ.py
+++ WYtoQQ.py
@@ -22,9 +22,9 @@ class WYtoQQ(BaseSpider):
         sleep(0.5)
         self.browser.find_element_by_id("switcher_plogin").click()
         user_input = self.browser.find_element_by_id("u")
-        user_input.send_keys(self.config.qq_account)
+        user_input.send_keys(self.config.account)
         pwd_input = self.browser.find_element_by_id("p")
-        pwd_input.send_keys(self.config.qq_password)
+        pwd_input.send_keys(self.config.password)
         submit = self.browser.find_element_by_id("login_button")
         submit.click()
         sleep(1)
@@ -36,7 +36,7 @@ class WYtoQQ(BaseSpider):
         print "login sucess"
 
     def get_source_playlist(self):
-        response = requests.get(self.config.wy_playlist_url, headers=headers)
+        response = requests.get(self.config.source_playlist_url.replace('#', 'm'), headers=headers)
         html = response.content
         soup = BeautifulSoup(html, "html.parser")
         details = soup.select("span[class='detail']")
@@ -54,19 +54,21 @@ class WYtoQQ(BaseSpider):
         self.source_playlist = song_details
 
     def get_target_playlist(self):
-        self.browser.get(qq_playlist_url)
-        self.wait.until(lambda browser: browser.find_element_by_class_name("playlist__list"))
-        playlist = self.browser.find_element_by_class_name("playlist__list")
-        playlist_items = playlist.find_elements_by_class_name('playlist__item')
-
-        for item in playlist_items:
-            title = item.find_element_by_class_name('playlist__title').text
-            item_id = item.get_attribute('data-dirid')
-            if title == self.config.qq_playlist_name:
-                self.target_playlist_tag = item_id
-                return
-        else:
-            raise Exception("can not find qq playlist:{}, please check".format(self.config.qq_playlist_name))
+        # self.browser.get(self.config.target_playlist_url)
+        # self.wait.until(lambda browser: browser.find_element_by_class_name("playlist__list"))
+        # playlist = self.browser.find_element_by_class_name("playlist__list")
+        # playlist_items = playlist.find_elements_by_class_name('playlist__item')
+        #
+        # for item in playlist_items:
+        #     title = item.find_element_by_class_name('playlist__title').text
+        #     item_id = item.get_attribute('data-dirid')
+        #     if title == self.config.qq_playlist_name:
+        #         self.target_playlist_tag = item_id
+        #         return
+        # else:
+        #     raise Exception("can not find qq playlist:{}, please check".format(self.config.qq_playlist_name))
+        self.target_playlist_tag = self.config.target_playlist_url.split('dirid=')[-1]
+        return
 
     def sync_song(self):
         for song_detail in self.source_playlist:
--- run.py
+++ run.py
@@ -1,19 +1,34 @@
 import platform
+import os
+import stat
+import traceback
+from utils import unzip
 
 download_url = {
     "Windows": "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-windows.zip",
     "Darwin": "https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-macosx.zip"  # Mac OS
 }
+driver_zip_file = {
+    "Windows": "phantomjs-2.1.1-macosx.zip",
+    "Darwin": "phantomjs-2.1.1-macosx.zip"
+}
 default_driver_path = {
-    "Windows": "phantomjs-2.1.1-windows/bin/phantomjs.exe",
-    "Darwin": "phantomjs-2.1.1-macosx/bin/phantomjs"  # Mac OS
+    "Windows": "driver/phantomjs-2.1.1-windows/bin/phantomjs.exe",
+    "Darwin": "driver/phantomjs-2.1.1-macosx/bin/phantomjs"  # Mac OS
 }
 current_os = platform.system()
 
 if __name__ == '__main__':
     try:
         import settings
-        driver_path = raw_input("Please input driver path: ")
+        if not os.path.exists(default_driver_path[current_os]):
+            zip_file_path = driver_zip_file[current_os]
+            if not os.path.exists(zip_file_path):
+                raise Exception("Please Download driver first!!!")
+            unzip(zip_file_path, 'driver')
+        driver_path = default_driver_path[current_os]
+        st = os.stat(driver_path)
+        os.chmod(driver_path, st.st_mode | stat.S_IEXEC)
         settings.phantomjs_driver_path = driver_path
         mode = raw_input("Please select sync mode(1: from WY to QQ; 2: from QQ to WY)")
         if mode in ("1", 1):
@@ -25,7 +40,7 @@ if __name__ == '__main__':
         else:
             raise Exception("Please select 1 or 2")
     except Exception as e:
-        print e
+        traceback.print_exc()
         raw_input("Error! press Enter to exit")
     else:
-        raw_input("finish")
+        raw_input("Finish! press Enter to exit")

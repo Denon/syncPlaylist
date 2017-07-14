3d5089041c6aa35b6647ca29f3f919da29740734
WDH <denonw@foxmail.com>
2017-07-14 20:38:13 +0800

    fix win32 bug

--- my_setup.py
+++ my_setup.py
@@ -7,6 +7,7 @@ setup(
     data_files=[
         (r'.', glob(r'D:\myproject\syncPlaylist\config.json')),
         (r'.', glob(r'D:\Anaconda3\envs\py27\Lib\site-packages\selenium\webdriver\remote\getAttribute.js')),
-        (r'.', glob(r'D:\Anaconda3\envs\py27\Lib\site-packages\selenium\webdriver\remote\isDisplayed.js'))
+        (r'.', glob(r'D:\Anaconda3\envs\py27\Lib\site-packages\selenium\webdriver\remote\isDisplayed.js')),
+        (r'.', glob(r'D:\myproject\syncPlaylist\phantomjs-2.1.1-windows.zip'))
     ]
 )
